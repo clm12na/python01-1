0bec98f875793061dcd598ede619482c2bb5eeae
Davidcorral94 <davidcp94@hotmail.com>
2015-03-14 14:53:25 +0100

    Versión simple
    Actualizado

--- entregable01DavidCorralPlaza.py
+++ entregable01DavidCorralPlaza.py
@@ -1,12 +1,5 @@
-def primeraestrofa(beers):
-     print beers,"bottles of beer on the wall,", beers,"bottles of beer."
-
-def segundaestrofa(beers):
-    print "Take one down, pass it around,",beers,"bottles of beer on the wall.\n"
-
 beers = 99
 while beers >= 1:
-    primeraestrofa(beers)
+    print beers,"bottles of beer on the wall,", beers,"bottles of beer."
     beers = beers - 1
-    segundaestrofa(beers)
-    
+    print "Take one down, pass it around,",beers,"bottles of beer on the wall.\n"
